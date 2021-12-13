fd02fe681f423594d55236fc0be485c463e83e2f
Yongho Kim <winthisgame12@gmail.com>
2021-12-13 14:17:05 -0500

    add login page

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './header.module.css'
 
 const Header = ({ onLogout }) => (
-    <header className={styles.header}>{onLogout && <button className={styles.logout} onClick={onLogout}>Logout</button>}
-    <img src="/images/logo.png" alt="logo" />
-    <h1>Business Card Maker</h1>
+    <header className={styles.header}>{true && <button className={styles.logout} onClick={onLogout}>Logout</button>}
+    <img className={styles.logo} src="/images/logo.png" alt="logo" />
+    <h1 className={styles.title} >Business Card Maker</h1>
 
     </header>
 );
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import styles from './login.module.css';
 
 const Login = ({ authService }) => {
 const onLogin = event => {
@@ -9,16 +10,16 @@ const onLogin = event => {
       .then(console.log);
   };
     return (
-        <section>
+        <section className={styles.login}>
             <Header />
                 <section>
                     <h1>Login</h1>
-                    <ul>
-                        <li>
-                            <button onClick={onLogin}>Google</button>
+                    <ul className={styles.list}>
+                        <li className={styles.item}>
+                            <button className={styles.button} onClick={onLogin}>Google</button>
                         </li> 
                         <li>
-                            <button onClick={onLogin}>Github</button>
+                            <button className={styles.button} onClick={onLogin}>Github</button>
                         </li>
                     </ul>
                 </section>
