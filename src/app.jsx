4155ad9f0a2c312ccbc814fddf0f25d01e194672
Yongho Kim <winthisgame12@gmail.com>
2021-12-13 12:34:26 -0500

    add gitHub google

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,8 @@
 import './app.css';
+import Login from './components/login/login';
 
-function App() {
-  return <h1>Hello :)</h1>;
+function App({authService}) {
+  return <Login authService={authService}/>;
 }
 
 export default App;
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,10 +1,40 @@
+// import firebase from 'firebase';
+// // import firebase from 'firebase/compat/app'
+// // import firebaseApp from './firebase'
+
+
+// // class AuthService {
+// //     login(providerName){
+// //         const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+// //         return firebaseApp.auth().signInWithPopup(authProvider);
+// //     }
+
+  
+// // }
+
+// // export default AuthService;
+
+// // import firebase from 'firebase';
+// import firebaseApp from './firebase';
+
+// class AuthService {
+//   login(providerName) {
+//     const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+//     return firebaseApp.auth().signInWithPopup(authProvider);
+//   }
+// }
+
 import firebase from 'firebase';
+import firebaseApp from './firebase';
 
 class AuthService {
-    login(providerName) {
-        const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-        return firebase.auth().signInwithPopup(authProvider);
-    }
+  login(providerName) {
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebaseApp.auth().signInWithPopup(authProvider);
+  }
 }
 
 export default AuthService;
+
+
+// export default AuthService;
